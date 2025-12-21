ad2bf9af9aee9697272e3ab2fde6f700156e310a
Venkatesh1499 <venkateshnimmalapudi100@gmail.com>
2025-12-21 09:43:14 -0800

    Initial push

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO, emit, join_room
+
+app = Flask(__name__)
+app.config['SECERT_KEY'] = "secret123"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@socketio.on("join_room")
+def handle_join(data):
+    room = data["room"]
+    join_room(room)
+    emit("room_message", {"message": f"Joined room {room}"}, room = room)
+
+@socketio.on("room_message")
+def handle_message(data):
+    print("message received", data["message"], data["room"])
+    room = data["room"]
+    message = data["message"]
+    emit("room_message", {"message":message}, room=room)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
